e4a0beeb61f9cd09ea0ac254c92b3d76c4fb1ad9
mainaksur14 <mainaksur14@gmail.com>
2021-05-13 19:30:51 +0530

    commit1

---
+++ src/main/java/com/xfactor/lably/entity/Test.java
@@ -0,0 +1,26 @@
+package com.xfactor.lably.entity;
+
+public class Test {
+    
+    private String name;
+    private String description;
+    private Double price;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
